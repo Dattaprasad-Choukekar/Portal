3eb7a374e890253c45b49dba50532ee3d9fe3b6e
mrajkumar83 <mrajkumar83@gmail.com>
2016-02-18 21:17:32 +0100

    removed conflicts

---
+++ public/js/angular/configurations/teacher.js
@@ -0,0 +1,20 @@
+var app = angular.module("TeacherPageApp", ['ngRoute']);
+
+app.config(function($routeProvider) {
+		
+        $routeProvider
+            .when('/', {
+                templateUrl : 'views/my_courses.html',
+                controller  : 'TeacherCtrl'
+            }).when('/course', {
+                templateUrl : 'views/course.html',
+                controller  : 'CourseCtrl'
+            }).when('/my_courses', {
+                templateUrl : 'views/my_courses.html',
+                controller  : 'TeacherCtrl'
+            });
+			
+			
+    });
+	
+var controllers = {};
